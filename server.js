70fe265eaf1e80f9e60025f7e84ebd380734b8ab
Uwihirwe <145299822+Uwihirwe@users.noreply.github.com>
2025-05-26 09:39:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Add cors package
 const employeeRoutes = require('./routes/routes');
 const authRoutes = require('./routes/authRoutes');
 const Joi = require('joi');
@@ -11,6 +12,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
 // Routes
 app.use('/api/auth', authRoutes);
